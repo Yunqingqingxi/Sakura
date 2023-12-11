84b4e00e0a8e7c045ab625a8f6a6586922f29d23
Yunqingqingxi <132554180+Yunqingqingxi@users.noreply.github.com>
2023-12-12 00:40:15 +0800

    更新 pl.java
    添加注释

--- src/main/java/com/example/demo1/pl.java
+++ src/main/java/com/example/demo1/pl.java
@@ -18,11 +18,19 @@ public class pl extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+/*
+*这是实现评论的servlet
+*通过comment和username
+*调用DBUtil.insertMessage2(name,content);方法实现
+*/
         req.setCharacterEncoding("UTF-8");
         resp.setContentType("text/html");
+        
         String content = req.getParameter("comment");
         String name = req.getParameter("username");
+
         try {
+            // 实现写评论
             DBUtil.insertMessage2(name,content);
             req.getRequestDispatcher("wzxq2.jsp").forward(req, resp);
         } catch (SQLException e) {
